5f04d2b1e923e078919acd14cce0074516a13237
Ragul CS <ragulcs15@gmail.com>
2021-01-20 19:15:33 +0530

    V1.0.1

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,32 @@ const openNavbar = () => {
 const closeNavbar = () => {
   collapsedNavItems.style.top = "-200%";
 };
+
+//IMAGE ANIMATION USING GSAP
+
+const t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".header",
+    start: "top",
+  },
+});
+
+t1.from("#watch-img", { delay: 0.5, x: 300, opacity: 0 });
+
+const t2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#watch-img",
+    start: "top",
+  },
+});
+
+t2.from("#learn-img", { delay: 0.5, x: -300, opacity: 0 });
+
+const t3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#learn-img",
+    start: "top",
+  },
+});
+
+t3.from("#explore-img", { delay: 0.5, x: 300, opacity: 0 });
