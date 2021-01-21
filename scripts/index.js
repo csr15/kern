4311d22cc0e3769157ca2866f04def142a4400f1
Ragul CS <ragulcs15@gmail.com>
2021-01-21 15:04:38 +0530

    Updated section titles

--- scripts/index.js
+++ scripts/index.js
@@ -8,35 +8,52 @@ const closeNavbar = () => {
 };
 
 // GSAP
-const t1 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".watch",
-    start: "center",
-  },
-});
-t1.from(
-  "#watch-img",
-  { opacity: 0, x: 300 },
-);
+if (window.screen.width <= 768) {
+  const t1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#triggerAnimation",
+      start: "top",
+    },
+  });
+  t1.from("#watch-img", { opacity: 0, x: 300, delay: 0 }, "-=1");
 
-const t2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".watch-section",
-    start: "center",
-  },
-});
-t2.from(
-  "#learn-img",
-  { opacity: 0, x: -300 },
-);
+  const t2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".watch-section",
+      start: "bottom",
+    },
+  });
+  t2.from("#learn-img", { opacity: 0, x: -300, delay: 0 }, "-=1");
 
-const t3 = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".learn-section",
-    start: "center",
-  },
-});
-t3.from(
-  "#explore-img",
-  { opacity: 0, x: 300 },
-);
+  const t3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".learn-section",
+      start: "bottom",
+    },
+  });
+  t3.from("#explore-img", { opacity: 0, x: 300, delay: 0 }, "-=1");
+} else {
+  const t1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".watch-videos",
+      start: "bottom",
+    },
+  });
+  t1.from("#watch-img", { delay: 0, x: 300, opacity: 0 }, "-=0.7");
+
+  const t2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".watch-section",
+      start: "center",
+    },
+  });
+  t2.from("#learn-img", { delay: 0, x: -300, opacity: 0 }, "-=0.7");
+
+  const t3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".learn-section",
+      start: "center",
+    },
+  });
+  t3.from("#explore-img", { delay: 0, x: 300, opacity: 0 }, "-=0.7");
+}
