4f7a759ddad191e254aa6145ff997534aaa49f32
Ragul CS <ragulcs15@gmail.com>
2021-01-21 13:19:53 +0530

    Updated section titles

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,39 @@ const closeNavbar = () => {
   collapsedNavItems.style.top = "-200%";
 };
 
-// ANIMATION INITIALIZATION
-window.onload = () => {
-  new WOW().init();
-};
+// GSAP
+const t1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".watch",
+    start: "center",
+  },
+});
+t1.from(
+  "#watch-img",
+  { opacity: 0, x: 300 },
+  "-=0.7"
+);
+
+const t2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".watch-section",
+    start: "center",
+  },
+});
+t2.from(
+  "#learn-img",
+  { opacity: 0, x: -300 },
+  "-=0.7"
+);
+
+const t3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".learn-section",
+    start: "center",
+  },
+});
+t3.from(
+  "#explore-img",
+  { opacity: 0, x: 300 },
+  "-=0.7"
+);
